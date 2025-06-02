9354977776bdf336c28c170b3d9eb3ed8058fdbb
Rachit <rachitkdas@gmail.com>
2025-06-02 18:46:24 +0000

    WIP: Config Implementation
    This config implementation is hardcoded.
    However, it is close to the one specified in the docs

--- raft/internal/config/config.go
+++ raft/internal/config/config.go
@@ -1,9 +1,34 @@
 package config
 
 import (
-   "fmt"
+	"encoding/json"
+	"fmt"
 )
 
-func Init() {
-	fmt.Println("Initialize Config Reader")
+type GlobalConfig struct {
+	numServers int
+	RPCMethod  string
+	serverInfo map[string]int
+}
+
+type LocalConfig struct {
+	LogHandler          string
+	StateMachineHandler string
+}
+
+func Init() (GlobalConfig, LocalConfig) {
+
+	fmt.Println("Initializing Config Reader")
+	a, _ := json.Marshal("1")
+	fmt.Println(a)
+	fmt.Println("reading configs from config file.")
+
+	// for now just init globalConfigs locally
+	// we will see about reading it from JSON later
+	tempMap := make(map[string]int)
+	clusterConfigs := GlobalConfig{5, "HTTPRPCHandler", tempMap}
+	localConfigs := LocalConfig{"TextHandler", "NoActionHandler"}
+	fmt.Println(clusterConfigs)
+
+	return clusterConfigs, localConfigs
 }
--- raft/main.go
+++ raft/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "github.com/RachitKeertiDas/raft/internal/config"
+	"fmt"
+
+	"github.com/RachitKeertiDas/raft/internal/config"
 )
 
-func main(){
-	fmt.Println("Hello. Initializing Raft Server")
-	config.Init()
+func main() {
 
+	fmt.Println("Hello. Initializing Raft Server")
+
+	//  Load configs
+	serverConfigs, localConfigs := config.Init()
+	fmt.Println(serverConfigs, localConfigs)
 }
