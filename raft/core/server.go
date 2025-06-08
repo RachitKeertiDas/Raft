7040c1a66d0cca012215dce52461e109c70e63c2
Rachit <rachitkdas@gmail.com>
2025-06-08 08:00:40 +0000

    Add More Core Logic

--- raft/core/server.go
+++ raft/core/server.go
@@ -1,29 +1,46 @@
 package core
 
 type Server struct {
-	state string
-	term  int
+	state    string
+	term     int
+	votedFor int
 }
 
 const electionTimeout = 150
+const heartbeatTimeout = 5
 
 func Init() Server {
 	// A server must initialize to a follower state.
 	// TODO: For now, initialize term to 0
 	// I think in case logger gets it back, it should increment the term
-	server := Server{"Follower", 0}
+	server := Server{"Follower", 0, -1}
 	return server
 }
 
 func Run(server *Server) {
+	// start the RPCListener
+	// This will be done in a separate GoRoutine
+	// This allows us to parallely check and transition to candidate if
+	// te server times out.
 
-	// keep waiting for queries until timeout
+	// go RPCListener.ListenForWriteRequests()
+	timeout := 2
+	if server.state == "Follower" {
+		for timeout < heartbeatTimeout && server.state == "Follower" {
+			// Keep Listening to queries and waiting for timeout
+			// Listening to queries will be done by the http handler.
 
-	var timeout = false
-	if timeout == true {
+		}
+	}
+
+	if server.state == "Candidate" {
 		startElection(server)
 	}
 
+	if server.state == "Leader" {
+		// logWrite Requests will be handled, replicated by the handler.
+		// the handler will change the state accordingly
+	}
 }
 
 func startElection(server *Server) {
@@ -31,4 +48,8 @@ func startElection(server *Server) {
 	// increment term
 	server.state = "Candidate"
 	server.term += 1
+
+	// server.RPCHandler.RequestVote()
+
+	// RPCServer.
 }
