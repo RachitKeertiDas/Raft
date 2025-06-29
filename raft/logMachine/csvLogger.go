6ffb2237c11a7ad9ba03ad38430455b1ea582706
Rachit <rachitkdas@gmail.com>
2025-06-29 17:50:49 +0000

    logMachine: Implement more of csvLogger

--- raft/logMachine/csvLogger.go
+++ raft/logMachine/csvLogger.go
@@ -2,8 +2,13 @@ package logMachine
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 type CsvLogger struct {
@@ -27,7 +32,15 @@ func (logger *CsvLogger) InitLogger() (int, int) {
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
-	fmt.Println(csvReader.Read())
+	headers, err := csvReader.Read()
+	if err != nil {
+		log.Printf("Unexpected Error while reading %s", err)
+	}
+	err = sanityCheckHeaders(headers)
+	if err != nil {
+		log.Println("Incorrect headers in CSV File: encountered error", err)
+		// Probably should delete the file and try again
+	}
 	return 0, 0
 }
 
@@ -52,13 +65,44 @@ func (logger *CsvLogger) FetchLatestLog() (int, int, string, error) {
 	f := logger.openFileForRead()
 	defer f.Close()
 
-	return 0, 0, "", nil
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		return -1, -1, "", err
+	}
+	// check the first record, it's the header
+	header := records[0]
+	sanityCheckHeaders(header)
+
+	rawRecords := records[1:]
+	// no entries in the file yet
+	if len(rawRecords) == 0 {
+		return 0, 0, "", nil
+	}
+
+	latestRecord := rawRecords[len(records)-1]
+
+	term, _ := strconv.Atoi(latestRecord[0])
+	index, _ := strconv.Atoi(latestRecord[1])
+	return term, index, latestRecord[2], nil
 }
 
 func (logger *CsvLogger) FetchLogWithIndex(index int) (int, string) {
 
 	f := logger.openFileForRead()
 	defer f.Close()
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		return -1, ""
+	}
+
+	for _, record := range records {
+		localIndex, _ := strconv.Atoi(record[0])
+		if localIndex == index {
+			log.Println("found index")
+		}
+	}
 
 	return 0, ""
 }
@@ -86,4 +130,14 @@ func (logger *CsvLogger) openFileForAppend() *os.File {
 		return nil
 	}
 	return f
+}
+
+func sanityCheckHeaders(headers []string) error {
+	var expectedHeaders = []string{"Term", "Index", "Entry"}
+
+	if slices.Equal(headers, expectedHeaders) {
+		return errors.New("Mismatch in headers, expected:" + strings.Join(expectedHeaders, ",") + "found:" + strings.Join(headers, ","))
+	}
+	return nil
+
 }
